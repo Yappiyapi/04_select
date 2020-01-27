aa70c1a2c8461af7866d362afb618c061779025f
Yappiyapi <yynissy.718@gmail.com>
2020-01-27 11:54:07 +0900

    修正

--- select.php
+++ select.php
@@ -1,9 +1,6 @@
 <?php
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 7df9bb5cff295b0a4913a15ec9014062519d61d4
 define('DSN', 'mysql:host=mysql;dbname=pet_shop;charset=utf8;');
 define('USER', 'staff');
 define('PASSWORD', '9999');
@@ -15,10 +12,7 @@ try {
   exit;
 }
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 7df9bb5cff295b0a4913a15ec9014062519d61d4
 $sql = "select * from animals";
 
 $stmt = $dbh->prepare($sql);
@@ -27,12 +21,7 @@ $stmt->execute();
 
 $animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-<<<<<<< HEAD
 foreach ($animals as $animal)
 {
-  echo $animal['type'] . 'の' . $animal['classifcation'] . 'ちゃん' . '<br>' .  $animal['description'] . '<br>' . $animal['birthday'] . '生まれ' . '<br>' . '出身地' .  ['birthday'] . '<hr>';
-=======
-foreach ($animals as $animal) {
-  echo $animal['type'] .  'の' . $animal['classifcation'] . 'ちゃん' .'<br>' . $animal['description'] . '<br>' . $animal['birthday'] . ' 生まれ' . '<br>' . '出身地 ' . $animal['birthplace'] . '<hr>';
->>>>>>> 7df9bb5cff295b0a4913a15ec9014062519d61d4
+  echo $animal['type'] . 'の' . $animal['classifcation'] . 'ちゃん' . '<br>' .  $animal['description'] . '<br>' . $animal['birthday'] . '生まれ' . '<br>' . '出身地' .  $animal['birthday'] . '<hr>';
 }
