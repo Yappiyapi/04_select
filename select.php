5311271587ad32d8ad1a1aa5c76472b9aac2bd9b
Yappiyapi <yynissy.718@gmail.com>
2020-01-30 02:28:04 +0900

    データベース課題2

--- select.php
+++ select.php
@@ -1,3 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>ペットショップ検索</title>
+</head>
+<body>
+  <h1>本日のご紹介ペット！</h1>
+  <p>
+    <form action="" method="post">
+      キーワード:
+      <input type="text" name="description" placeholder="キーワードの入力">
+      <input type="submit" value="検索">
+    </form>
+  </p>
+</body>
+</html>
+
 <?php
 
 define('DSN', 'mysql:host=mysql;dbname=pet_shop;charset=utf8;');
@@ -11,7 +31,7 @@ try {
   exit;
 }
 
-$sql = "select * from animals";
+$sql = "select * from animals where description like '%おっとり%'";
 
 $stmt = $dbh->prepare($sql);
 
@@ -22,3 +42,9 @@ $animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
 foreach ($animals as $animal) {
   echo $animal['type'] .  'の' . $animal['classifcation'] . 'ちゃん' .'<br>' . $animal['description'] . '<br>' . $animal['birthday'] . ' 生まれ' . '<br>' . '出身地 ' . $animal['birthplace'] . '<hr>';
 }
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $description = $_POST['description'];
+};
+
+?>
