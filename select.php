f92618a8184df5d7952916dc24a7d8171901f1bb
Yappiyapi <yynissy.718@gmail.com>
2020-02-02 03:34:44 +0900

    foreachがerror

--- select.php
+++ select.php
@@ -1,5 +1,28 @@
-<?php
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>ペットショップ検索</title>
+</head>
+<body>
+  <h1>本日のご紹介ペット！</h1>
+  <p>
+    <form action="" method="GET">
+      <label for="keyword">キーワード:
+      <input type="text" name="keyword" placeholder="キーワードの入力">
+      </label>
+      <input type="submit" value="検索">
+    </form>
+    <?php foreach ($animals as $animal): ?>
+    <?php echo h($animls['description']);?>
+    <?php endforeach; ?>
+  </p>
+</body>
+</html>
 
+<?php
 
 define('DSN', 'mysql:host=mysql;dbname=pet_shop;charset=utf8;');
 define('USER', 'staff');
@@ -12,16 +35,27 @@ try {
   exit;
 }
 
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+  $keyword = $_GET["keyword"];
 
-$sql = "select * from animals";
+  if ($keyword == "") {
+    $sql = "select * from animals";
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+    $animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
 
-$stmt = $dbh->prepare($sql);
-
-$stmt->execute();
-
-$animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($animals as $animal)
-{
-  echo $animal['type'] . 'の' . $animal['classifcation'] . 'ちゃん' . '<br>' .  $animal['description'] . '<br>' . $animal['birthday'] . '生まれ' . '<br>' . '出身地' .  $animal['birthplace'] . '<hr>';
+  if ($keyword = '%'.$keyword.'%'); {
+    $sql2 = "select * from animals where description LIKE %おっとり% :keyword";
+    $stmt = $dbh->prepare($sql2);
+    $stmt->bindParam(':keyword', $keyword);
+    $stmt->execute();
+    $animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  }
 }
+
+  foreach ($animals as $animal) {
+  echo $animal['type'] .  'の' . $animal['classifcation'] . 'ちゃん' .'<br>' . $animal['description'] . '<br>' . $animal['birthday'] . ' 生まれ' . '<br>' . '出身地 ' . $animal['birthplace'] . '<hr>';
+  }
+
+?>
