e92049fb9eac88332490c1f297ff684cf1c9f4dc
Yappiyapi <yynissy.718@gmail.com>
2020-02-03 16:31:49 +0900

    課題2終わりました

--- select.php
+++ select.php
@@ -1,27 +1,3 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>ペットショップ検索</title>
-</head>
-<body>
-  <h1>本日のご紹介ペット！</h1>
-  <p>
-    <form action="" method="GET">
-      <label for="keyword">キーワード:
-      <input type="text" name="keyword" placeholder="キーワードの入力">
-      </label>
-      <input type="submit" value="検索">
-    </form>
-    <?php foreach ($animals as $animal): ?>
-    <?php echo h($animls['description']);?>
-    <?php endforeach; ?>
-  </p>
-</body>
-</html>
-
 <?php
 
 define('DSN', 'mysql:host=mysql;dbname=pet_shop;charset=utf8;');
@@ -41,21 +17,39 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
   if ($keyword == "") {
     $sql = "select * from animals";
     $stmt = $dbh->prepare($sql);
+  } else {
+    $sql = "select * from animals where description LIKE :keyword";
+    $stmt = $dbh->prepare($sql);
+    $keyword = '%'.$keyword.'%';
+    $stmt->bindParam(":keyword", $keyword);
+  }
     $stmt->execute();
     $animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  }
-
-  if ($keyword = '%'.$keyword.'%'); {
-    $sql2 = "select * from animals where description LIKE %おっとり% :keyword";
-    $stmt = $dbh->prepare($sql2);
-    $stmt->bindParam(':keyword', $keyword);
-    $stmt->execute();
-    $animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  }
 }
 
-  foreach ($animals as $animal) {
-  echo $animal['type'] .  'の' . $animal['classifcation'] . 'ちゃん' .'<br>' . $animal['description'] . '<br>' . $animal['birthday'] . ' 生まれ' . '<br>' . '出身地 ' . $animal['birthplace'] . '<hr>';
-  }
 
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>ペットショップ検索</title>
+</head>
+<body>
+  <h1>本日のご紹介ペット！</h1>
+  <p>
+    <form action="" method="GET">
+      <label for="keyword">キーワード:
+      <input type="text" name="keyword" placeholder="キーワードの入力">
+      </label>
+      <input type="submit" value="検索">
+    </form>
+    <?php foreach ($animals as $animal) : ?>
+    <?php echo ($animal['type'] .  'の' . $animal['classifcation'] . 'ちゃん' .'<br>' . $animal['description'] . '<br>' . $animal['birthday'] . ' 生まれ' . '<br>' . '出身地 ' . $animal['birthplace'] . '<hr>'); ?>
+    <?php endforeach; ?>
+  </p>
+</body>
+</html>
